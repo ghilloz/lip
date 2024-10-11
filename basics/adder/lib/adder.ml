b3299848cc4406b29771c6f8befd30f4a7c9559e
ghilloz <killmy9755@gmail.com>
2024-10-11 12:56:36 +0200

    Adder

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,5 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let addlist l = List.fold_left (fun x y -> x+y) 0 l;;
+
